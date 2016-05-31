55d22aa08710a24f13d8951deaf8f9ef804e7a6d
peter <peter@pflrs.com>
2016-05-30 20:27:20 -0700

    v0.02 Cleaned up pyInput.py

--- pythonArduino/pyInput.py
+++ pythonArduino/pyInput.py
@@ -1,14 +1,25 @@
 print 'Welcome to the program'
 
 print 'This is catch and repeat'
-print 'Press \'x\' to exit'
+print 'Press \'q\' to exit'
 
 x = False
 
 while True:
 	character = raw_input("Type a character:")
-	print character
-	if character == 'x'
+	if character is 'r':
+		print "Red"
+	elif character is 'g':
+		print "Green"
+	elif character is 'b':
+		print "Blue"
+	elif character is '1':
+		print "White"
+	elif character is '0':
+		print "Off"
+	elif character is 'q':
+		print "Fizz"
 		break
-
+	else:
+		print "Enter Valid character (r, g, b, 1, 0), q to exit"
 print 'Exiting Program'
