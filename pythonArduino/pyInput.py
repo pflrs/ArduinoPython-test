dffb3c8481ec44f92d4b8793c3dcf36a8eb41fc0
peter <peter@pflrs.com>
2016-05-30 22:33:23 -0700

    pyInput.py working with serial
    only works with hardcoded serial ports (Linux/Mac)
    need to add scanning USB serial ports

--- pythonArduino/pyInput.py
+++ pythonArduino/pyInput.py
@@ -1,7 +1,23 @@
+import sys
+import serial
+import time
+import serial.tools.list_ports
+
 print 'Welcome to the program'
 
 print 'This is catch and repeat'
 print 'Press \'q\' to exit'
+
+ports = list(serial.tools.list_ports.comports())
+for p in ports:
+	print p
+	if "USB UART" in p[1]:
+		print "USB UART detected"
+
+delaytime = 1
+#ser = serial.Serial('/dev/cu.usbserial-DC008I9I', 9600)
+ser = serial.Serial('/dev/ttyUSB0', 9600)
+time.sleep(2) # delay for 2 seconds
 
 x = False
 
@@ -9,14 +25,19 @@ while True:
 	character = raw_input("Type a character:")
 	if character is 'r':
 		print "Red"
+		ser.write('r')
 	elif character is 'g':
 		print "Green"
+		ser.write('g')
 	elif character is 'b':
 		print "Blue"
+		ser.write('b')
 	elif character is '1':
 		print "White"
+		ser.write('1')
 	elif character is '0':
 		print "Off"
+		ser.write('0')
 	elif character is 'q':
 		print "Fizz"
 		break
